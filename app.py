a7f8982359e0dd259a57d2624d8102aa1b302770
Adarsh Kumar <60352729+AdarshIITDH@users.noreply.github.com>
2026-04-19 17:31:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from Cloud Build vired🚀 This is version 2 of the app"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
